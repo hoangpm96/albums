98794f53f4a15708d31ccb8b222c974d2887da02
hoangpm <hoangpm.qn96@gmail.com>
2017-09-27 16:35:50 +0700

    Add new style, add image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,18 +4,24 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album;
-
+    const { title, artist, thumbnail_image, image } = album;
+    const { headerContentStyle, headerTextStyle, 
+        cardContentStyle, thumbnailStyle, 
+        thumbnailContainerStyle, imageStyle } = styles;
+   
     return (
         <Card>
-            <CardSection style={styles.cardContentStyle}>
-                <View>
-                    <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+            <CardSection style={cardContentStyle}>
+                <View style={thumbnailContainerStyle}>
+                    <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
                 </View>
-                <View style={styles.headerContentStyle}>
-                    <Text>{title}</Text>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                <Image style={imageStyle} source={{ uri: image }} />
             </CardSection>
         </Card>
     );
@@ -26,6 +32,9 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+   headerTextStyle: {
+        fontSize: 20
+   },
     cardContentStyle: {
         flexDirection: 'row',
         justifyContent: 'space-around'
@@ -33,6 +42,17 @@ const styles = {
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+         justifyContent: 'center',
+         alignItems: 'center',
+         marginLeft: 10,
+         marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
